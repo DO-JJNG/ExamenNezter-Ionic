a67ea494829e12fcb5a3ac2f93a1cb7bddac194c
Julián Navia <82625175+DO-JJNG@users.noreply.github.com>
2023-06-13 10:40:27 -0700

    Add files via upload

---
+++ ExamenNezterAPI/Controllers/usuarios.cs
@@ -0,0 +1,94 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ExamenNezterAPI.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class usuarios : ControllerBase
+    {
+
+        private readonly IDbConnection _conexion;
+        public usuarios(IConfiguration config)
+        {
+            _conexion = new SqlConnection(config.GetConnectionString("DefaultConnection"));
+
+        }
+
+        // Get users -------------------------------------
+
+        [HttpGet("GetUsuarios")]
+
+        public async Task<ActionResult<List<usuariosN>>> GetAllUsuarios()
+        {
+            //var users = await _conexion.QueryAsync<usuariosN>("GetAllUsers", CommandType.StoredProcedure);
+            //return Ok(users);
+            var users = await _conexion.QueryAsync<usuariosN>("select * from USUARIOS");
+            return Ok(users);
+        }
+        //-------------------------------------------------
+
+        [HttpGet("ById/{idusuario}")]
+
+        public async Task<ActionResult<usuariosN>> SelectIdUser(int idusuario)
+        {
+
+            var users = await _conexion.QueryAsync<usuariosN>("SelectIdUser",new 
+            { @idusuario = idusuario }, commandType: CommandType.StoredProcedure);
+            return Ok(users);
+        }
+        //------------------------------------------------- 
+        [HttpPost("create")]
+
+        public async Task<ActionResult<usuariosN>> CreateUsuario(usuariosN NewUser)
+        {
+           var users= await _conexion.ExecuteAsync("CreateUsuario", new
+           {
+              
+               @nombre= NewUser.nombre,
+               @direccion= NewUser.direccion,
+               @telefono= NewUser.telefono,
+               @codigoP= NewUser.codigoP,
+               @estado= NewUser.estado,
+               @ciudad= NewUser.ciudad,
+               @TipoU = NewUser.TipoU,
+
+           }, commandType: CommandType.StoredProcedure);
+            return Ok(users);
+        }
+
+        //------------------------------------------------- revisar
+           
+        [HttpPut("actualizar")]
+
+        public async Task<ActionResult<List<usuariosN>>> UpdateUsuario(usuariosN NewUser)
+        {
+
+
+            var users = await _conexion.ExecuteAsync("UpdateUsuarios", new
+            {
+                @idusuario = NewUser.idusuario,
+                @nombre = NewUser.nombre,
+                @direccion = NewUser.direccion,
+                @telefono = NewUser.telefono,
+                @codigoP = NewUser.codigoP,
+                @estado = NewUser.estado,
+                @ciudad = NewUser.ciudad,
+                @TipoU = NewUser.TipoU,
+                 
+            }, commandType: CommandType.StoredProcedure);
+            return Ok(users);   
+        }
+        //---------------------------------------------
+        [HttpGet("eliminar/{idusuario}")] 
+        public async Task<ActionResult<List<usuariosN>>> DeleteUsuario(int idusuario) 
+        {
+            var users = await _conexion.ExecuteAsync("DeleteUsuario", new { idusuario = @idusuario }, commandType: CommandType.StoredProcedure);
+            return Ok(users);
+        }
+    }
+}
---
+++ ExamenNezterAPI/usuarios.cs
@@ -0,0 +1,22 @@
+﻿namespace ExamenNezterAPI
+{
+    public class usuariosN
+    {
+        public int idusuario { get; set; }
+        public string nombre { get; set; }
+
+        public string direccion { get; set;}
+
+        public string telefono { get; set; }
+
+        public string codigoP { get; set; }
+
+        public string estado { get; set; }
+
+        public string ciudad { get; set; }
+
+        public string TipoU { get; set; }
+
+
+    }
+}
